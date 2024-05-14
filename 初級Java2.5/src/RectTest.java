9d7ca1b4c644b24af29f793cbc630096f3b2320d
mok <jojy0628@gmail.com>
2024-05-14 12:15:36 +0900

    3.3　コミット

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,10 +1,17 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		setColor(255,0,255);
 		fillRect(130,40,50,100);
-		fillRect(30,200,150,10);
-		fillRect(90,160,30,30);
+		setColor(80,208,255);
+		fillRect(230,40,50,100);
+		setColor(0,0,0);
+		fillRect(330,40,50,100);
+		setColor(64,64,64);
+		fillRect(30,240,50,100);
+		
 		
 	}
 
