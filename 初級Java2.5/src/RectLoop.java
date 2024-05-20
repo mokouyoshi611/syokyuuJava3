183f771463784c66717fba98d64eaab46fa9ee58
mok <jojy0628@gmail.com>
2024-05-20 12:20:31 +0900

    確認4.4

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -3,10 +3,22 @@ public class RectLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
 		int x=30;
-		while (x<=170) {
-			System.out.println(x);	
-			fillRect(x,80,10,100);
+		int i;
+		for (i=0 ; i<9 ; i++) {
+			
+			if(i==0) {
+				fillRect(x,60,10,100);
+			}
+			else if(i==8) {
+				fillRect(x,100,10,100);
+			}
+			else {
+				fillRect(x,80,10,100);
+			}
+
+
 			x+=20;
+
 		}
 	}
 
