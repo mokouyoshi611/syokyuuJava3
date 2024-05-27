56f9ae6f1355ae8196f03c8ef45f8e0857bb95e8
mok <jojy0628@gmail.com>
2024-05-27 10:14:13 +0900

    5.9

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -2,12 +2,14 @@
 public class Animation extends MyFrame{
 	public void run(){
 		int y = 100;
+		int x=30;
 		while (y<=200) {
 			clear();
 			
 			setColor(0,128,0);
-			fillRect(30,y,10,100);
+			fillRect(x,y,10,100);
 			y+=5;
+			x+=5;
 
 			sleep(0.1);
 		}
