21afd143331e91cd47ccf2a470f0e0fefd6df2ae
mok <jojy0628@gmail.com>
2024-05-27 09:55:56 +0900

    5.2確認問題

---
+++ 初級Java2.5/src/Animation.java
@@ -0,0 +1,16 @@
+
+public class Animation extends MyFrame{
+	public void run() {
+		int x = 30;
+		while (x<=170) {
+			clear();
+			
+			setColor(0,128,0);
+			fillOval(x,80,50,50);
+			x+=20;
+			sleep(1);
+		}
+	}
+	
+
+}
