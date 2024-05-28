3b3768e7937a480d9686803f9b7d84d1f9ae4bf1
mok <jojy0628@gmail.com>
2024-05-28 10:56:51 +0900

    6-5

--- 初級Java2.5/src/start.java
+++ 初級Java2.5/src/start.java
@@ -2,8 +2,7 @@ public class start {
 
 	public static void main(String[] args) {
 	   System.out.println("プログラムを実行します");
-	   MyFrame frame1=new DisplayFaceObject();
-
+	   MyFrame frame1=new DisplayfaceAndTree();
 	}
 
 }
