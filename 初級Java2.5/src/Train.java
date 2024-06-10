34eea862a2a12c154e01af05dd5517295c7841c9
mok <jojy0628@gmail.com>
2024-06-10 10:43:27 +0900

    8.3

--- 初級Java2.5/src/Train.java
+++ 初級Java2.5/src/Train.java
@@ -1,11 +1,8 @@
 
-public class Train {
-	int x,y,vx,vy;
+public class Train extends Vehicle {
+	
 	public Train(int x,int y,int vx,int vy) {
-		this.x=x;
-		this.y=y;
-		this.vx=vx;
-		this.vy=vy;
+	super(x,y,vx,vy);
 		
 	}
 	public void draw(MyFrame frame) {
